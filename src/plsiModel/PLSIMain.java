eaccbcac1f93876d3583aa96f32a1f2332c6665e
Tuan-AnhPC\Huyen Nguyen <nthuyen.bmth@gmail.com>
2017-09-20 11:02:34 +0200

    update model

--- src/plsiModel/PLSIMain.java
+++ src/plsiModel/PLSIMain.java
@@ -1,127 +1,187 @@
-package plsiModel;
-
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
-import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Random;
+import java.util.Scanner;
 
 public class PLSIMain {
 	public static void main(String[] args) {
 		int nOfTopics = 20;
 		int nOfDocs = 269;
 		int nOfWords = 975;
-		//HashSet<Integer> topics = new HashSet<Integer>();
-		HashMap<Integer, HashMap<Integer, Double>> dataSet = new HashMap<Integer, HashMap<Integer, Double>>();
-		double[][] groudTruthTheta = new double[nOfDocs][nOfTopics];
+		// HashSet<Integer> topics = new HashSet<Integer>();
+		HashMap<Integer, HashMap<Integer, Integer>> dataSet = new HashMap<Integer, HashMap<Integer, Integer>>();
+
 		System.out.println("Generate synthetic data: ");
-		getSyntheticData(dataSet, nOfTopics, nOfDocs, nOfWords);
+		double[][][] groudTruthTheta = getSyntheticData(dataSet, nOfTopics, nOfDocs, nOfWords);
+		System.out.println("Training model:");
 		PLSIModel plsi = new PLSIModel(dataSet, nOfWords, nOfTopics, nOfDocs);
+		System.out.println("->> initialize parameters:");
 		plsi.init();
+		System.out.println("->> update parameters:");
+		plsi.trainingModel();
+	//	System.out.println("Complete!");
 		
+		/*double[][][] learntTheta = plsi.getLearntTheta();
+		Scanner scan = new Scanner(System.in);
+		for(int z = 0; z<nOfTopics; z++) {
+			for(int d = 0; d<nOfDocs; d++) {
+				for(int w = 0; w<nOfWords; w++) {
+					System.out.println(learntTheta[z][d][w]+"\t"+groudTruthTheta[z][d][w]);
+					scan.nextLine();
+				}
+			}
+		}*/
 	}
 
-	public static double[][] getSyntheticData(HashMap<Integer, HashMap<Integer, Double>> dataSet, int nOfTopics, int nOfDocs, int nOfWords) {
-		Random rand = new Random();
+	public static double[][][] getSyntheticData(HashMap<Integer, HashMap<Integer, Integer>> dataSet,
+			 int nOfTopics, int nOfDocs, int nOfWords) {
+
 		double[] pDocs = new double[nOfDocs];
-		double[][] pTopicDocs = new double[nOfTopics][nOfDocs];
-		double[][] pWordTopics = new double[nOfWords][nOfTopics];
+		double[][] pZD = new double[nOfTopics][nOfDocs];
+		double[][] pWZ = new double[nOfWords][nOfTopics];
+		double[][][] pZDW = new double[nOfTopics][nOfDocs][nOfWords];
 		// select a document with a random probability
 
 		double sum = 0;
-		for (int i = 0; i < nOfDocs; i++) {
-			pDocs[i] = rand.nextDouble();
-			sum += pDocs[i];
-		}
-		for (int i = 0; i < nOfDocs; i++) {
-			pDocs[i] = pDocs[i] / sum;
+		for (int d = 0; d < nOfDocs; d++) {
+			pDocs[d] = 1 / nOfDocs;
+
 		}
 
 		// pick a latent class with a random probability
-		for (int i = 0; i < nOfDocs; i++) {
-			sum = 0;
-			for (int j = 0; j < nOfTopics; j++) {
-				pTopicDocs[j][i] = rand.nextDouble();
-				sum += pTopicDocs[j][i];
+		for (int d = 0; d < nOfDocs; d++) {
+			double pros[] = getRandomProbabilities(nOfTopics);
+			for (int z = 0; z < nOfTopics; z++) {
+				pZD[z][d] = pros[z];
+
 			}
-			for (int j = 0; j < nOfTopics; j++) {
-				pTopicDocs[j][i] = pTopicDocs[j][i] / sum;
-			}
+
 		}
 		// generate a word with a random probability
-		for (int i = 0; i < nOfWords; i++) {
-			for (int j = 0; j < nOfTopics; j++) {
-				pWordTopics[i][j] = Math.random();
+		for (int z = 0; z < nOfTopics; z++) {
+			double[] pros = getRandomProbabilities(nOfWords);
+			for (int w = 0; w < nOfWords; w++) {
+				pWZ[w][z] = pros[w];
+				//System.out.println(pWZ[w][z]);
 			}
 
 		}
 
-		for (int i = 0; i < nOfDocs; i++) {
-			HashMap<Integer, Double> wordList = new HashMap<Integer, Double>();
-			for (int j = 0; j < nOfWords; j++) {
-				double p = pDocs[i];
+		// sampling from a multinomial distribution
+		// get P(w|d)
+		HashMap<Integer, HashMap<Integer, Double>> pWD = new HashMap<Integer, HashMap<Integer, Double>>();
+		for (int d = 0; d < nOfDocs; d++) {
+			HashMap<Integer, Double> wordProsMap = new HashMap<Integer, Double>();
+			for (int w = 0; w < nOfWords; w++) {
+				sum = 0;
 				for (int z = 0; z < nOfTopics; z++) {
-					p += pWordTopics[j][z]*pTopicDocs[z][i];
+					sum += pWZ[w][z] * pZD[z][d];
+					//System.out.println(sum);
 				}
-				if (p > 0)
-					wordList.put(j, p);
+				wordProsMap.put(w, sum);
 			}
-			dataSet.put(i, wordList);
+			pWD.put(d, wordProsMap);
 		}
-		return pTopicDocs;
+
+		// sampling 
+		for(int d = 0; d<nOfDocs; d++) {
+			HashMap<Integer, Integer> wordCountMap = new HashMap<Integer, Integer>();
+			HashMap<Integer, Double> wordProsMap = pWD.get(d);
+			
+			//sort probabilities in increasing order
+			List<Entry<Integer, Double>> sortedWordProsMap = new ArrayList<Entry<Integer, Double>>(wordProsMap.entrySet());
+			Collections.sort(sortedWordProsMap, new Comparator<Map.Entry<Integer, Double>>() {
+
+				@Override
+				public int compare(Entry<Integer, Double> o1, Entry<Integer, Double> o2) {
+					// TODO Auto-generated method stub
+					return o1.getValue().compareTo(o2.getValue());
+				}
+				
+			});
+			
+			Random rand = new Random();
+			double[] cdf = new double[nOfWords];
+			cdf[0] = sortedWordProsMap.get(0).getValue();
+			for(int i = 1; i<nOfWords; i++) {
+				cdf[i]=cdf[i-1]+sortedWordProsMap.get(i).getValue();
+			}
+			
+			int nOfWordInDocument = rand.nextInt(nOfWords-1)+1; 
+			for(int i = 0; i<nOfWordInDocument; i++) {
+				 int r = Math.abs(Arrays.binarySearch(cdf, rand.nextDouble()/100));
+				 if(r== sortedWordProsMap.size()) r--;
+				 int word = sortedWordProsMap.get(r).getKey();
+				 if(wordCountMap.containsKey(word)) {
+					 int count = wordCountMap.get(word);
+					 wordCountMap.put(word, count+1);
+				 } else
+					 wordCountMap.put(word, 1);
+			}
+			
+		
+			dataSet.put(d, wordCountMap);
+		}
+		for(Map.Entry<Integer, HashMap<Integer, Integer>> docx: dataSet.entrySet()) {
+			HashMap<Integer, Integer> words = docx.getValue();
+			
+			for(Map.Entry<Integer, Integer> wordCounts: words.entrySet()) {
+				System.out.println("d: "+docx.getKey()+"\tw: " + wordCounts.getKey()+"\t"+ wordCounts.getValue());
+			}
+		}
+		
+		// get groundTruth
+		
+		for(int d = 0; d<nOfDocs; d++) {
+			for(int w = 0; w<nOfWords; w++) {
+				if(!dataSet.get(d).containsKey(w))
+					continue;
+				sum = 0;
+				for(int z = 0; z<nOfTopics; z++) {
+					pZDW[z][d][w] = pZD[z][d] * pWZ[w][z];
+					sum += pZDW[z][d][w];
+				}
+				for(int z = 0; z<nOfTopics; z++) {
+					pZDW[z][d][w] /= sum;
+					
+				}
+			}
+		}
+		
+		
+		return pZDW;
 	}
 
-	/*public static HashMap<String, HashMap<String, String>> getDataSet(String path) {
-		HashMap<String, HashMap<String, String>> dataSet = new HashMap<String, HashMap<String, String>>();
-		try {
-			BufferedReader buff = new BufferedReader(new FileReader(path));
-			String line = null;
-			HashMap<String, String> docCountMap = new HashMap<String, String>();
-			String elementsInLine[] = new String[3];
-			while ((line = buff.readLine()) != null) {
-				elementsInLine = line.split("\\s");
-				if (!dataSet.containsKey(elementsInLine[0])) {
-					docCountMap = new HashMap<String, String>();
-					docCountMap.put(elementsInLine[1], elementsInLine[2]);
-					dataSet.put(elementsInLine[0], docCountMap);
-				} else {
-					docCountMap = dataSet.get(elementsInLine[0]);
-					docCountMap.put(elementsInLine[1], elementsInLine[2]);
-				}
-			}
-			buff.close();
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+	/*public static HashMap<Integer, HashMap<Integer, Integer>> getSampleData(double[][] pWD) {
+		HashMap<Integer, HashMap<Integer, Integer>> dataSet = new HashMap<Integer, HashMap<Integer, Integer>>();
+		for (int d = 0; d < pWD[0].length; d++) {
+			
 		}
 		return dataSet;
-	}
+	}*/
 
-	public static HashSet<String> getTopicIds(String path) {
-		HashSet<String> topicIds = new HashSet<String>();
-		try {
-			BufferedReader buff = new BufferedReader(new FileReader(path));
-			String line = null;
-			while ((line = buff.readLine()) != null) {
-				topicIds.add(line);
-			}
-
-			buff.close();
-
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			System.exit(-1);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+	// get a normalize array
+	public static double[] getRandomProbabilities(int length) {
+		double[] pros = new double[length];
+		double sum = 0;
+		Random rand = new Random();
+		for (int i = 0; i < length; i++) {
+			pros[i] = rand.nextDouble();
+			sum += pros[i];
+		}
+		for (int i = 0; i < length; i++) {
+			pros[i] = pros[i] / sum;
 		}
 
-		return topicIds;
-	}*/
+		return pros;
+	}
+
+
 }
--- src/plsiModel/PLSIModel.java
+++ src/plsiModel/PLSIModel.java
@@ -1,20 +1,20 @@
-package plsiModel;
 
 import java.util.HashMap;
 import java.util.Random;
 
 public class PLSIModel {
-	private HashMap<Integer, HashMap<Integer, Double>> dataSet;
+	private HashMap<Integer, HashMap<Integer, Integer>> dataSet;
 	private int nOfTopics, nOfWords, nOfDocs;
-	private double[] pZ;
 	private double[][] pDZ;
 	private double[][] pWZ;
+	private double[] pZ;
+	private double[][][] pZDW;
 
 	public PLSIModel() {
 		// TODO Auto-generated constructor stub
 	}
 
-	public PLSIModel(HashMap<Integer, HashMap<Integer, Double>> inputData, int nOfWords, int nOfTopics, int nOfDocs) {
+	public PLSIModel(HashMap<Integer, HashMap<Integer, Integer>> inputData, int nOfWords, int nOfTopics, int nOfDocs) {
 		dataSet = inputData;
 		this.nOfDocs = nOfDocs;
 		this.nOfTopics = nOfTopics;
@@ -22,82 +22,143 @@ public class PLSIModel {
 		pZ = new double[nOfTopics];
 		pDZ = new double[nOfDocs][nOfTopics];
 		pWZ = new double[nOfWords][nOfTopics];
+		pZDW = new double[nOfTopics][nOfDocs][nOfWords];
+		
+	}
+	public double[][][] getLearntTheta() {
+		return pZDW;
+	}
+	public void trainingModel() {
+		double sum;
+		for (int i = 0; i < 20; i++) {
+
+			// E-step: Expectation step
+			System.out.println("+ E step:");
+			for (int d = 0; d < nOfDocs; d++) {
+				for (int w = 0; w < nOfWords; w++) {
+					if (!dataSet.get(d).containsKey(w))
+						continue;
+					sum = 0;
+					for (int z = 0; z < nOfTopics; z++) {
+						
+						pZDW[z][d][w] = pZ[z] * pDZ[d][z] * pWZ[w][z];
+						sum += pZDW[z][d][w];
+					}
+					for (int z = 0; z < nOfTopics; z++) {
+						pZDW[z][d][w] /= sum;
+						//System.out.println(pZDW[z][d][w]);
+					}
+				}
+			}
+
+			// M-step: Maximization step
+			System.out.println("+ M step:");
+			// update p(w|z) = sum_d(n(d, w)*p(z|d, w))/sum_d(sum_w(n(d, w')*p(z|d, w')))
+			for (int z = 0; z < nOfTopics; z++) {
+				sum = 0;
+				for (int w = 0; w < nOfWords; w++) {
+					for (int d = 0; d < nOfDocs; d++) {
+						if (!dataSet.get(d).containsKey(w))
+							continue;
+						pWZ[w][z] += dataSet.get(d).get(w) * pZDW[z][d][w];
+					
+					}
+					sum += pWZ[w][z];
+					
+				
+				}
+				for (int w = 0; w < nOfWords; w++) {
+					
+					pWZ[w][z] /= sum;
+					
+				}
+
+			}
+
+			// update p(d|z) = sum(n(d, w')*p(z|d, w'))/sum(n(d', w')*p(z|d', w'))
+			for (int z = 0; z < nOfTopics; z++) {
+				sum = 0;
+				for (int d = 0; d < nOfDocs; d++) {
+					for (int w = 0; w < nOfWords; w++) {
+						if (!dataSet.get(d).containsKey(w))
+							continue;
+						pDZ[d][z] += dataSet.get(d).get(w) * pZDW[z][d][w];
+					}
+					sum += pDZ[d][z];
+				}
+				for (int d = 0; d < nOfDocs; d++) {
+					
+					pDZ[d][z] /= sum;
+				}
+			}
+			// update p(z) = sum(sum(n(d', w')P(z|d',w'))
+			for(int z = 0; z<nOfTopics; z++) {
+				sum = 0;
+				for(int d = 0; d<nOfDocs; d++) {
+					for(int w = 0; w<nOfWords; w++) {
+						if(!dataSet.get(d).containsKey(w))
+							continue;
+						pZ[z] += dataSet.get(d).get(w) * pZDW[z][d][w];
+						sum+= dataSet.get(d).get(w);
+					}
+				}
+				pZ[z] /= sum;
+			}
+
+		}
 
 	}
 
 	public void init() {
-		System.out.println("Initialize Parameters:");
-		initializeParameters();
-		printParameters();
-		trainingModel();
-	}
-	
-	public void trainingModel() {
-		/*double[][][] pZDW = new double[][][];
-		for(int i = 0; i<1000; i++) {
-			//E-step: Expectation step
-			for(int t = 1; t<nOfTopics; t++) {
-				for()
-			}
-			//M-step: Maximization step
-			for(int i = 1; i<nOfTopics; i++) {
-				for()
-			}
-		}
-		*/
-			    
-			  
-	}
-	public void initializeParameters() {
-		// randomly assign cluster for each pair of doc-word
 		Random ran = new Random();
-		HashMap<Integer, HashMap<Integer, Integer>> clusters = new HashMap<Integer, HashMap<Integer, Integer>>();
-		int nOfRow = 0;
-		for(int i = 0; i<dataSet.size(); i++) {
-			// iterate each word in current document
-			for(int j = 0; j<dataSet.get(i).size(); j++) {
-				int cluster = ran.nextInt(nOfTopics);
-				pZ[cluster]++;
-				nOfRow++;
-				pDZ[i][cluster]++;
-				pWZ[j][cluster]++;
-			}
-			for(int j = 0; j<nOfTopics; j++) {
-				pDZ[i][j] = pDZ[i][j]/dataSet.get(i).size();
-			}
+		double sum;
+
+		// init p(z)
+		for (int z = 0; z < nOfTopics; z++) {
+			pZ[z] = (double)1 / nOfTopics;
 		}
-		
-		for(int i = 0; i<nOfTopics; i++) {
-			pZ[i] = pZ[i]/nOfRow;
-		}
-		
-		int sum;
-		for(int i = 0; i<nOfWords; i++) {
+
+		// int pDZ
+		for (int z = 0; z < nOfTopics; z++) {
 			sum = 0;
-			for(int j = 0; j<nOfTopics; j++) {
-				sum +=pWZ[i][j];
+			for (int d = 0; d < nOfDocs; d++) {
+				pDZ[d][z] = ran.nextDouble();
+				sum += pDZ[d][z];
 			}
-			for(int j = 0; j<nOfTopics; j++) {
-				pWZ[i][j] = pWZ[i][j]/sum;
+			for (int d = 0; d < nOfDocs; d++) {
+				pDZ[d][z] /= sum;
+			}
+
+		}
+		// init p(w|z),for each topic the constraint is sum(p(w|z))=1.0
+		for (int z = 0; z < nOfTopics; z++) {
+			sum = 0;
+			for (int w = 0; w < nOfWords; w++) {
+				pWZ[w][z] = ran.nextDouble();
+				sum += pWZ[w][z];
+			}
+			for (int w = 0; w < nOfWords; w++) {
+				pWZ[w][z] /= sum;
 			}
 		}
-		
+
 	}
+
 	public void printParameters() {
 		System.out.println("PZ:");
-		for(int i = 0; i<nOfTopics; i++)
-			System.out.println("Cluster: " +i+": "+pZ[i]);
+		for (int i = 0; i < nOfTopics; i++)
+			System.out.println("Cluster: " + i + ": " + pZ[i]);
 		System.out.println("............................................");
 		System.out.println("PDZ:");
-		for(int i = 0; i<nOfDocs; i++) {
-			for(int j = 0; j<nOfTopics; j++) {
-				System.out.print(pDZ[i][j]+"\t");
+		for (int i = 0; i < nOfDocs; i++) {
+			for (int j = 0; j < nOfTopics; j++) {
+				System.out.print(pDZ[i][j] + "\t");
 			}
 			System.out.println();
 		}
-		for(int i = 0; i<nOfWords; i++) {
-			for(int j = 0; j<nOfTopics; j++) {
-				System.out.print(pWZ[i][j]+"\t");
+		for (int i = 0; i < nOfWords; i++) {
+			for (int j = 0; j < nOfTopics; j++) {
+				System.out.print(pWZ[i][j] + "\t");
 			}
 			System.out.println();
 		}
